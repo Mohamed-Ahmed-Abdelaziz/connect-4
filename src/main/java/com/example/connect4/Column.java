e56582d8f463a2e9d84fa32b12c2fce3150bc24e
hassanAhmed310 <h01552405179@gmail.com>
2023-11-11 13:47:58 +0200

    Added column functions definitions

--- src/main/java/com/example/connect4/Column.java
+++ src/main/java/com/example/connect4/Column.java
@@ -1,4 +1,11 @@
 package com.example.connect4;
 
 public class Column {
+    short values;
+    public boolean add(){
+        return false;
+    }
+    public int get(int index){
+        return 0;
+    }
 }
