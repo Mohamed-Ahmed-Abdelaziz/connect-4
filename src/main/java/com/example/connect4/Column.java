68156bc146de7a091340ffd2e30d1e9bbb126af9
hassanAhmed310 <h01552405179@gmail.com>
2023-11-11 22:32:35 +0200

    Added column functions bodies

--- src/main/java/com/example/connect4/Column.java
+++ src/main/java/com/example/connect4/Column.java
@@ -1,11 +1,40 @@
 package com.example.connect4;
 
 public class Column {
-    short values;
-    public boolean add(){
-        return false;
+    // cell value = 0 for empty, 1 for pc, 2 for human
+    byte length;
+    byte filledCells;
+    short cells;
+
+    public Column(){
+        length = 7;
+        cells = 0;
+        filledCells = 0;
     }
+
+    public short getCells() {
+        return cells;
+    }
+
+    public boolean add(int turn){
+        if(filledCells == length || turn < 1 || turn > 2){
+            return false;
+        }
+        short input = (short) (turn << filledCells*2);
+        cells = (short) (cells | input);
+        filledCells++;
+        return true;
+    }
+
     public int get(int index){
-        return 0;
+        if(index < 0 || index >= length){
+            return -1;
+        }
+        int mask = 3 << index * 2;
+        return (mask & (int)cells) >> (index * 2);
+    }
+
+    public boolean isFilled(){
+        return filledCells == length;
     }
 }
